db65e7dcfd99563b52290d2a54a66c3133ef1ae5
Groza-Frontline <151484854+Groza-Frontline@users.noreply.github.com>
2026-09-23 17:09:14 +0800

    Made a function to destroy Target (gameobject)

--- The Orbital Interceptor/Assets/Scripts/OrbitalTarget.cs
+++ The Orbital Interceptor/Assets/Scripts/OrbitalTarget.cs
@@ -4,9 +4,16 @@ public class OrbitalTarget : MonoBehaviour
 {
     public Transform pointA, pointB;
     public float travelDuration;
+    public SphereCollider targetObject;
 
     private float timer;
     private bool toPointB = true;
+
+    //Used Awake to access the Sphere Collider(Target) to destroy it
+    private void Awake()
+    {
+        targetObject = GetComponent<SphereCollider>();
+    }
 
     // Update is called once per frame
     void Update()
@@ -15,7 +22,7 @@ public class OrbitalTarget : MonoBehaviour
         timer += Time.deltaTime;
         float t = timer / travelDuration;
 
-        //Short version of the if else statement
+        //2. Short version of the if else statement
         Vector3 start = toPointB ? pointA.position : pointB.position;
         Vector3 target = toPointB ? pointB.position : pointA.position;
 
@@ -29,5 +36,15 @@ public class OrbitalTarget : MonoBehaviour
             // toPointB = false;
             toPointB = !toPointB;
         }
+
+        //5. Bypasses manual raycasts
+        Input.GetMouseButtonDown(0);
+    }
+
+    public void OnMouseDown()
+    {
+        //If clicked
+        Destroy(gameObject);
+        Debug.Log("Target destroyed!");
     }
 }
